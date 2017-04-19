18697422dc4bfb1628c653285a8d6e9b8607a7ef
TOMV-PC\TomV <tom.vanmechelen@libis.be>
2017-04-19 14:38:48 +0200

    Thomas More colors

--- primo-explore/custom/ECB/js/custom.js
+++ primo-explore/custom/ECB/js/custom.js
@@ -3,47 +3,21 @@
 'use strict';
 
 var app = angular.module('viewCustom', ['angularLoad']);
-
-app.component('prmLogoAfter', {
-    bindings: {},
-    template: '<div class="hello-world"><span>Hello World</span></div>'
-});
 /*
- * Thumbnail links demo
- * Ulrich Leodolter <ulrich.leodolter@obvsg.at>
- */
-app.controller('SearchResultThumbnailContainerAfterController', ['angularLoad', function (angularLoad) {
-    var vm = this;
+app.component('prmUserAreaAfter', {
+    bindings: {
+        parentCtrl: '<'
+    },
+    controller: function($compile, $scope, $templateCache, $element) {
+      $templateCache.put('components/search/topbar/userArea/user-area.html', `
+          <div layout='row' layout-align="center center">
+            <prm-authentication layout="flex" [is-logged-in]="$ctrl.userName().length > 0"></prm-authentication>
+            <prm-change-lang aria-label="{{'eshelf.signin.title' | translate}}" ng-if="$ctrl.displayLanguage" label-type="icon"></prm-change-lang>
+            <prm-library-card-menu></prm-library-card-menu>
+          </div>`);
 
-    /*
-     * Get central catalog unique identifier
-     */
-
-    console.log(vm.parentCtrl.item.pnx.control);
-    vm.acnumber = '';
-    if ('addsrcrecordid' in vm.parentCtrl.item.pnx.control) {
-        vm.acnumber = vm.parentCtrl.item.pnx.control.addsrcrecordid[0];
+      $compile($element.parent())($scope);
     }
-    /*
-     * Get fulltext and table-of-contents pnx.links.linkstorsrc links
-     */
-    vm.thumb_links = [];
-    if (vm.parentCtrl.links.length > 0) {
-        vm.parentCtrl.links.forEach(function (link, i, links) {
-            if (link.linkType == 'http://purl.org/pnx/linkType/linktorsrc') {
-                if (link.displayLabel.match(/(Fulltext|Volltext)/)) {
-                    vm.thumb_links.push([link.linkURL, 'Volltext']);
-                } else if (link.displayLabel.match(/(Table of Contents|Inhaltsverzeichnis)/)) {
-                    vm.thumb_links.push([link.linkURL, 'Inhalts-verzeichnis']);
-                }
-            }
-        });
-    }
-}]);
-
-app.component('prmSearchResultThumbnailContainerAfter', {
-    bindings: { parentCtrl: '<' },
-    controller: 'SearchResultThumbnailContainerAfterController',
-    template: '<div ng-if="$ctrl.acnumber"><small>{{$ctrl.acnumber}}</small></div>\n        <div ng-repeat="link in $ctrl.thumb_links">\n           <a href="{{link[0]}}">{{link[1]}}</a>\n        </div>'
-});
+})
+*/
 })();
