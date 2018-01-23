a90b462202c6dbeceb819000e878ff614da53cdc
Tom Vanmechelen <tom.vanmechelen@libis.be>
2018-01-23 20:37:48 +0100

    DOKS and ECB

--- primo-explore/custom/ECB/js/custom.js
+++ primo-explore/custom/ECB/js/custom.js
@@ -3,29 +3,54 @@
 'use strict';
 
 var app = angular.module('viewCustom', ['angularLoad']);
-/*
-app.controller('prmUserAreaAfterController', ['$scope', '$element', '$location', '$mdDialog', '$compile', '$templateCache', function ($scope, $element, $location, $mdDialog, $compile, $templateCache) {
-    var self = this;
-    self.$onInit = function () {
-        $compile($element.parent())($scope);
+/* https://otago.hosted.exlibrisgroup.com/primo-explore/search?vid=DUNEDIN&sortby=rank&search_scope=All */
+
+/* TODO : get this on Central Package level */
+
+app.component('prmLinkedUserSelectorAfter', {
+    bindings: {
+        parentCtrl: '<'
+    },
+    controller: 'prmLinkedUserSelectorAfter',
+    templateUrl: 'custom/ECB/html/prmLinkedUserSelectorAfter.html'
+});
+
+app.controller('prmLinkedUserSelectorAfter', ['$element', '$scope', '$location', '$templateCache', function ($element, $scope, $location, $templateCach) {
+    var ctrl = this;
+    var scope = $scope;
+    var vid = scope.$parent.$parent.$ctrl.vid;
+
+    $scope.signOut = function () {
+        document.location.href = "https://leuven-primo.hosted.exlibrisgroup.com/pds?func=logout&calling_system=primo&institute=ECB&url=" + document.location.origin + "/primo-explore/search?vid=" + vid + "%26performLogout=true";
     };
-
-    $templateCache.put('components/search/topbar/userArea/user-area.html', `
-            <div layout='row' layout-align="center center">
-            <prm-change-lang aria-label="{{'eshelf.signin.title' | translate}}" ng-if="$ctrl.displayLanguage && !$ctrl.userName()" label-type="icon"></prm-change-lang>
-            <prm-authentication layout="flex"[is-logged-in]="$ctrl.userName().length > 0"></prm-authentication>
-            <prm-library-card-menu ng-if="$ctrl.userName()"></prm-library-card-menu>
-            </div>
-             `
-  )
-
 }]);
 
 app.component('prmUserAreaAfter', {
     bindings: {
         parentCtrl: '<'
     },
-    controller: 'prmUserAreaAfterController'
+    controller: 'prmUserAreaAfterController',
+    templateUrl: 'custom/ECB/html/user-area.html'
 });
-*/
+
+app.controller('prmUserAreaAfterController', ['$element', '$scope', '$location', '$templateCache', function ($element, $scope, $location, $templateCach) {
+
+    var ctrl = this;
+    ctrl.$onInit = function () {
+        ctrl.$element = $element;
+    };
+
+    ctrl.$postLink = function () {
+        ctrl.insertUserSignin();
+    };
+
+    ctrl.insertUserSignin = function () {
+        var elementRemove = document.getElementsByTagName('prm-user-area')[0];
+        angular.element(elementRemove.children[1]).remove();
+        angular.element(elementRemove.children[0]).remove();
+        $scope.signIn = function () {
+            ctrl.parentCtrl.loginIframeService.loginService.handleLoginClick();
+        };
+    };
+}]);
 })();
