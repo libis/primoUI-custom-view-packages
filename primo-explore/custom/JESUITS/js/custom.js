bf26830800b6909bf7609df565c1d39f5d5e122d
Tom Vanmechelen <tom.vanmechelen@libis.be>
2019-11-14 10:23:05 +0100

    View  JESUITS

---
+++ primo-explore/custom/JESUITS/js/custom.js
@@ -0,0 +1,8 @@
+(function(){
+"use strict";
+'use strict';
+
+var app = angular.module('viewCustom', ['angularLoad']);
+
+//.constant('facetsFilters', { name: "libary", values: ["BIBC"] });
+})();
---
+++ primo-explore/custom/JESUITS/js/custom.module.js
@@ -0,0 +1,3 @@
+var app = angular.module('viewCustom', ['angularLoad'])
+
+//.constant('facetsFilters', { name: "libary", values: ["BIBC"] });
