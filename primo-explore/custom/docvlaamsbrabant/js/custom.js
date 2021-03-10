276e0cd5b8c879c472928c19ae3834cf7a4a876a
JorisLambrechts <joris.lambrechts@kuleuven.be>
2021-03-10 10:54:36 +0100

    add aanwinsten link

--- primo-explore/custom/docvlaamsbrabant/js/custom.js
+++ primo-explore/custom/docvlaamsbrabant/js/custom.js
@@ -3,6 +3,32 @@
 'use strict';
 
 var app = angular.module('viewCustom', ['angularLoad']);
+app.component('prmAanwinsten', {
+    bindings: { parentCtrl: '<' },
+    controller: 'prmAanwinstenController',
+    template: '\n         <p>\n           Bekijk <a href="{{url}}">hier</a> welke publicaties de voorbije maand werden toegevoegd aan de catalogus\n         </p>\n'
+});
+
+app.controller("prmAanwinstenController", ['$scope', '$http', function ($scope, $http) {
+    var self = this;
+    var vid = window.appConfig['vid'];
+    var now = new Date();
+    var year = now.getFullYear();
+    console.log("Current year is: " + year);
+    var month = now.getMonth() + 1;
+    if (month - 1 == 0) {
+        month = 12;
+        year = year - 1;
+    } else {
+        month = month - 1;
+    }
+    month = ("0" + month).slice(-2);
+
+    console.log("Current month is: " + month);
+
+    $scope.url = window.location.origin + "/primo-explore/search?query=any,contains,acquisitionDate" + year + month + "DOCVB*,AND&tab=phys_items_tab&search_scope=PHYS_ITEMS&vid=docvlaamsbrabant&lang=nl_BE&mode=advanced&offset=0;";
+}]);
+
 var feeds = [{
     feedUrl: "https://limo-libis.blogspot.com/feeds/posts/default",
     feedLang: ['en_US'],
---
+++ primo-explore/custom/docvlaamsbrabant/js/prm-aanwinsten.js
@@ -0,0 +1,30 @@
+app.component('prmAanwinsten', {
+    bindings: { parentCtrl: '<' },
+    controller: 'prmAanwinstenController',
+    template: `
+         <p>
+           Bekijk <a href="{{url}}">hier</a> welke publicaties de voorbije maand werden toegevoegd aan de catalogus
+         </p>
+`
+});
+
+app.controller("prmAanwinstenController", ['$scope', '$http', function ($scope, $http) {
+    var self = this;
+    var vid = window.appConfig['vid'];
+    var now = new Date();
+    var year = now.getFullYear()
+    console.log("Current year is: " + year);
+    var month = now.getMonth() + 1;
+    if(month - 1 == 0){
+      month = 12;
+      year = year - 1;
+    }else{
+      month = month - 1;
+    }
+    month = ("0" + month).slice(-2);
+
+    console.log("Current month is: " + month);
+
+    $scope.url = window.location.origin + "/primo-explore/search?query=any,contains,acquisitionDate" + year + month + "DOCVB*,AND&tab=phys_items_tab&search_scope=PHYS_ITEMS&vid=docvlaamsbrabant&lang=nl_BE&mode=advanced&offset=0;"
+
+}]);
