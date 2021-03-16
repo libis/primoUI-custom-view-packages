b75f21284bc4846d9836a1bfb43ff59a23c313f4
JorisLambrechts <joris.lambrechts@kuleuven.be>
2021-03-16 09:56:44 +0100

    add sckcen

---
+++ primoVE/SCKCEN/32SCKCEN_INST-32SCKCEN_INST/js/custom.js
@@ -0,0 +1,15 @@
+(function(){
+"use strict";
+'use strict';
+
+var app = angular.module('viewCustom', ['angularLoad']);
+
+"use strict";
+'use strict';
+
+"use strict";
+'use strict';
+
+"use strict";
+'use strict';
+})();
