69bdc639c3c71149c24a0337791b23e347c56cc0
Tom Vanmechelen <tom.vanmechelen@libis.be>
2021-10-21 11:22:13 +0200

    redirect to PrimoVE

--- primo-explore/custom/ECB/js/custom.module.js
+++ primo-explore/custom/ECB/js/custom.module.js
@@ -1,1 +1,12 @@
 ﻿var app = angular.module('viewCustom', ['angularLoad']);
+
+
+
+
+/* Redirect for PrimoVE ECB */
+if ( window.location.search.match(/[?&]vid=ECB[&]?/) ){
+    var url = "https://ecb." + window.location.hostname + window.location.pathname + window.location.search
+    // console.log ( url  )
+    window.location.href = url
+  }
+  
